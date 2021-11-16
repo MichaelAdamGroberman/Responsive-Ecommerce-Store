b0eea554b2d018ee7e94c67d07293c3805a8e3ea
Michael Adam Groberman <michael.groberman@me.com>
2021-11-15 23:02:52 -0500

    please

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,10 +1,14 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || 'server', {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-  useCreateIndex: true,
-  useFindAndModify: false
-});
+mongoose.connect(
+  process.env.MONGODB_URI ||
+    'sermongodb+srv://mgroberman:Tu3sday29@cluster0.toizs.mongodb.net/E-Commerce?retryWrites=true&w=majorityver',
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+  }
+);
 
 module.exports = mongoose.connection;
