c1e7d50d6f0fd0934cbde2968f0b9e22748588e8
Michael Adam Groberman <81723568+MichaelAdamGroberman@users.noreply.github.com>
2021-11-16 00:02:07 -0500

    Update connection.js

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb+srv://mgroberman:Tu3sday29@cluster0.toizs.mongodb.net/E-Commerce?retryWrites=true&w=majority', {
+mongoose.connect(process.env.MONGODB_URI || 'mongoDB_server' {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useCreateIndex: true,
