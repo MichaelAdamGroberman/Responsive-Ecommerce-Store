abaaf9ae12f0292e355709516602a321f9f3909c
Michael Adam Groberman <michael.groberman@me.com>
2021-11-15 23:16:57 -0500

    trying to make it work

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,14 +1,10 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(
-  process.env.MONGODB_URI ||
-    'sermongodb+srv://mgroberman:Tu3sday29@cluster0.toizs.mongodb.net/E-Commerce?retryWrites=true&w=majorityver',
-  {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    useCreateIndex: true,
-    useFindAndModify: false,
-  }
-);
+mongoose.connect(process.env.MONGODB_URI || 'mongodb+srv://mgroberman:Tu3sday29@cluster0.toizs.mongodb.net/E-Commerce?retryWrites=true&w=majority', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
+  useFindAndModify: false
+});
 
 module.exports = mongoose.connection;
